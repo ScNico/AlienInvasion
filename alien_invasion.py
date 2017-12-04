58fb629fb3fc4d7b75277866bedf4d9e039f4f0f
ScNico <guangwei_chen@yeah.net>
2017-12-04 16:36:59 +0800

    Finish

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,6 +11,8 @@ from settings import Settings
 from ship import Ship
 import game_function as gf
 from game_stats import GameStats
+from button import Button
+from scoreboard import Scoreboard
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
@@ -19,22 +21,24 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    play_button = Button(ai_settings, screen, "Play")
+
     ship = Ship(ai_settings, screen)
     bullets = Group()
     aliens = Group()
     stats = GameStats(ai_settings)
-
+    sb = Scoreboard(ai_settings, screen, stats)
     #创建外星人群
     gf.create_fleet(ai_settings, screen, ship, aliens)
 
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
 
         if stats.game_active:
             ship.update()
-            gf.update_bullets(aliens, bullets)
-            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
 run_game()
--- game_stats.py
+++ game_stats.py
@@ -7,8 +7,11 @@ class GameStats():
         self.ai_settings = ai_settings
         self.reset_stats()
 
-        #游戏开始处于活动状态
-        self.game_active = True
+        #游戏开始处于非活跃状态
+        self.game_active = False
+        self.high_score = 0
 
     def reset_stats(self):
         self.ships_left = self.ai_settings.ship_limit
+        self.score = 0
+        self.level = 1
